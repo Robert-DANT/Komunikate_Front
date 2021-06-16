fe273f7865d1280cd7509167e7fc87cbf5421093
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2021-06-16 13:55:33 +0200

    add user cards v1

---
+++ src/components/Card/UserCard.js
@@ -0,0 +1,58 @@
+import { Card, Button, CardDeck, Container, Row } from "react-bootstrap";
+
+const UserCard = () => {
+  return (
+    <Container>
+      <Row>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Button variant="success">Chat</Button>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+              <Button variant="success">Chat</Button>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+              <Button variant="success">Chat</Button>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+      </Row>
+    </Container>
+  );
+};
+
+export default UserCard;
