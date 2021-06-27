871c19caa8a3cfe286fc640fd3e99e4c32581b7d
Jasmin262 <80625569+Jasmin262@users.noreply.github.com>
2021-06-27 16:02:17 +0200

    strat template

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 // import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-// import ArticleTemp from "./components/ArticlePage/ArticleTemp";
+import ArticleTemp from "./components/ArticlePage/ArticleTemp";
 
 const App = () => {
-  const [posts, setPosts] = useState();
-  // const [city, setCity] = useState(data.city);
+  //   const [posts, setPosts] = useState();
+  //   // const [city, setCity] = useState(data.city);
 
-  console.log(posts);
-  // console.log(city);
+  //   console.log(posts);
+  //   // console.log(city);
 
-  const fetchData = async () => {
-    await axios
-      .get(`https://jsonplaceholder.typicode.com/posts`)
-      .then((res) => setPosts(res.data.data))
-      .catch((err) => console.log(err));
-  };
+  //   const fetchData = async () => {
+  //     await axios
+  //       .get(`https://jsonplaceholder.typicode.com/posts`)
+  //       .then((res) => setPosts(res.data.data))
+  //       .catch((err) => console.log(err));
+  //   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  //   useEffect(() => {
+  //     fetchData();
+  //   }, []);
 
   return (
-   <>
-   <h1>{data}</h1>
-   </>
-  )
+    <>
+      <ArticleTemp />
+    </>
+  );
+};
+
 export default App;
