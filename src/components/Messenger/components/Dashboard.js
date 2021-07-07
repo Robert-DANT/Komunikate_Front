e0eee894781b5e391ab704bda99770bddc59200e
Rodan <77335057+Robert-DANT@users.noreply.github.com>
2021-07-07 12:11:16 +0200

    New Push to branch with Dashboard.js Head conflict resolved

--- src/components/Messenger/components/Dashboard.js
+++ src/components/Messenger/components/Dashboard.js
@@ -8,11 +8,7 @@ export default function Dashboard({ idUser }) {
     console.log(idUser)
     
     return (
-<<<<<<< HEAD
         <div className='d-flex' style={{ height: '90vh' }}>
-=======
-        <div className='d-flex' style={{ height: '80vh' }}>
->>>>>>> 160b85d5b7b10dd2d4054d91cceedd900150fb88
             <Sidebar idUser={idUser}/>
             {selectedConversation && <OpenConversations/>}
         </div>        
